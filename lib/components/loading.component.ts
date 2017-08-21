a48caa39d9e7b7a7b62070834dca24bde016b8ed
GaspardC <gaspard.chevassus-agnes@phelma.grenoble-inp.fr>
2017-08-21 13:29:35 +0200

    remove loading

--- lib/components/loading.component.ts
+++ lib/components/loading.component.ts
@@ -6,7 +6,7 @@ import { deprecatedSelector } from '../deprecated-selector';
   encapsulation: ViewEncapsulation.None,
   selector: 'LoadingComponent, tree-loading-component',
   template: `
-    <span *ngIf="!template">loading...</span>
+    <!--<span *ngIf="!template">loading...</span>-->
     <ng-container
       [ngTemplateOutlet]="template"
       [ngOutletContext]="{ $implicit: node }">
